c195eab5b5c291d9a922d1b0cf208b6bd2c8a1a0
Aart04 <jacekdembowiak@gmail.com>
2021-01-18 10:42:07 +0100

    added comand for taking a photo

--- DogIdentification/ViewModels/MainPageViewModel.cs
+++ DogIdentification/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace DogIdentification.ViewModels
 {
     class MainPageViewModel
     {
-    }
+        public MainPageViewModel()
+        {
+            TakePhotoCommand = new Command(() =>
+            {
+                //Taking Photo logic
+            });
+        }
+
+
+        public Command TakePhotoCommand { get; }
+    }   
 }
