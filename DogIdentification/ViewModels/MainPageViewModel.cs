c1cf376e43b73b8411bf22fec66a41fd55f3ca73
Aart04 <jacekdembowiak@gmail.com>
2021-01-18 11:13:06 +0100

    added take a photo task

--- DogIdentification/ViewModels/MainPageViewModel.cs
+++ DogIdentification/ViewModels/MainPageViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
 
 namespace DogIdentification.ViewModels
 {
@@ -17,5 +19,20 @@ namespace DogIdentification.ViewModels
 
 
         public Command TakePhotoCommand { get; }
+
+        async Task<System.IO.Stream> TakePhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                var stream = await photo.OpenReadAsync();
+                return stream;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"There was error during making a photo: {ex.Message}");
+                return null;
+            }
+        }
     }   
 }
