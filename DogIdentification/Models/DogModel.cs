4fb92e88ce6bc8e34aaee6b87300baa1a1281437
Aart04 <jacekdembowiak@gmail.com>
2021-01-19 09:09:14 +0100

    added dog and prediction model data

---
+++ DogIdentification/Models/DogModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogIdentification.Models
+{
+    public class DogModel
+    {
+        public string Breed { get; set; }
+    }
+}
---
+++ DogIdentification/Models/PredictionModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogIdentification.Models
+{
+   public class PredictionResult
+    {
+        public List<Classification> Predictions { get; set; }
+    }
+
+    public class Classification
+    {
+        public String TagName { get; set; }
+        public float Probability { get; set; }
+
+        public Classification(string tagName, float probability)
+        {
+            TagName = tagName;
+            Probability = probability;
+        }
+    }
+}
