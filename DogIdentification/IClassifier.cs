90efbd47df8f5045d804fab7d0ea4f13654124f1
Aart04 <jacekdembowiak@gmail.com>
2021-01-19 09:20:15 +0100

    added classification interface and classification completed event

---
+++ DogIdentification/IClassifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DogIdentification.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DogIdentification
+{
+    public interface IClassifier
+    {
+        event EventHandler<ClassificationEventArgs> ClassificationCompleted;
+
+        Task Classify(byte[] bytes);
+    }
+
+    public class ClassificationEventArgs : EventArgs
+    {
+        public List<Classification> Predictions { get; private set; }
+
+        public ClassificationEventArgs(List<Classification> predictions)
+        {
+            Predictions = predictions;
+        }
+    }
+}
