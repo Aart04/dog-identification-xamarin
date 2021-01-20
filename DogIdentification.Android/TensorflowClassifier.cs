37a29d6c1021c1fe0c31d205e1954b67f3297ab7
Aart04 <jacekdembowiak@gmail.com>
2021-01-20 18:23:14 +0100

    added function reading model .tflite to ByteBuffer

--- DogIdentification.Android/TensorflowClassifier.cs
+++ DogIdentification.Android/TensorflowClassifier.cs
@@ -9,6 +9,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Android.Content.Res;
+using Java.IO;
+using Java.Nio;
+using Java.Nio.Channels;
+using System.IO;
 
 namespace DogIdentification.Droid
 {
@@ -16,9 +21,26 @@ namespace DogIdentification.Droid
     {
         public event EventHandler<ClassificationEventArgs> ClassificationCompleted;
 
-        public Task Classify(byte[] bytes)
+        public async Task Classify(byte[] bytes)
         {
-            throw new NotImplementedException();
+            var modelByteBuffer = LoadModel("dog-identification"); 
+            var interpreter = new Xamarin.TensorFlow.Lite.Interpreter(modelByteBuffer);
+
+            var inputTensor = interpreter.GetInputTensor(0);
+
+            var shape = inputTensor.Shape();
+            
+        }
+            
+        private ByteBuffer LoadModel(string path)
+        {
+            var assetDescriptor = Application.Context.Assets.OpenFd(path);
+            var stream = new FileInputStream(assetDescriptor.FileDescriptor);
+
+            ByteBuffer byteBuffer = ByteBuffer.Allocate(stream.Available());
+            var channel = stream.Channel.Read(byteBuffer);
+            
+            return byteBuffer; 
         }
     }
 }
