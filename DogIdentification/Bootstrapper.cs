abd292f6db81885ed3c28a75932b589a388748f0
Aart04 <jacekdembowiak@gmail.com>
2021-01-21 22:30:28 +0100

    added Bootstrapper class

---
+++ DogIdentification/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace DogIdentification
+{
+    public static class Bootstrapper
+    {
+        public static IBootstrapper PlatformSpecific;
+
+        public static void Init(Application app)
+        {
+            var builder = new ContainerBuilder();
+
+            PlatformSpecific.Init(builder);
+
+
+            var container = builder.Build();
+            DependencyResolver.ResolveUsing(type => container.IsRegistered(type) ? container.Resolve(type) : null);
+        }
+    }
+
+    public interface IBootstrapper
+    {
+        void Init(ContainerBuilder builder);
+    }
+}
