a9465b03402e1e70aed4ae75b8f0c3b448a34eb0
Aart04 <jacekdembowiak@gmail.com>
2021-01-21 22:31:26 +0100

    added Bootstraper impementation in android project

---
+++ DogIdentification.Android/AndroidBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DogIdentification.Droid
+{
+    class AndroidBootstrapper : IBootstrapper
+    {
+        public void Init(ContainerBuilder builder)
+        {
+            builder.RegisterType<TensorflowClassifier>().Keyed<IClassifier>("OfflineInceptionModel");
+        }
+    }
+}
